54c3b5e46c5f580cdc4a5bb3b38cd7e69f6f90f7
Arturo <orutrax@gmail.com>
2018-08-27 01:15:50 -0300

    Validaciones de los Modelos Olvidadasgit add .!

--- app/models/family_goal.rb
+++ app/models/family_goal.rb
@@ -1,2 +1,53 @@
 class FamilyGoal < ApplicationRecord
+  # Antes de validar los campos convertimos los parametros a Mayusculas
+  before_validation :upcase_fields
+
+  # Creamos los campos permitidos para la Familia de Metas
+  @name = [
+    'EJECUTIVO PERSONAS', 'AGENTE', 'JEFE OPERACIONES',
+    'EJECUTIVO SELECT', 'GESTOR COMERCIAL SENIOR', 'EJECUTIVO PYME'
+  ]
+
+  @position = [
+    'EJECUTIVO PERSONAS', 'AGENTE I', 'AGENTE I', 'AGENTE II',
+    'EJECUTIVO SELECT', 'GESTOR COMERCIAL SENIOR', 'EJECUTIVO PYME'
+  ]
+
+  @area = [
+    'ZONA SUR', 'ZONA NORTE', 'ZONA CENTRO'
+  ]
+
+  @world = [
+    'SANTANDER', 'BANEFE', 'BANCO Y FILIALES'
+  ]
+
+  validate :family_exist
+  # Validamos los parametros
+  validates :name, inclusion: { in: @name,
+    message: "Debe seleccionar uno de los siguientes nombres #{ @name }" }
+
+  validates :position, inclusion: { in: @position,
+    message: "Debe seleccionar uno de los siguientes cargos #{ @position }" }
+
+  validates :area, inclusion: { in: @area,
+    message: "Debe seleccionar uno de los siguientes areas #{ @area }" }
+
+  validates :world, inclusion: { in: @world,
+    message: "Debe seleccionar uno de los siguientes mundos #{ @world }" }
+
+  # Convertimos los parametros en Mayusculas
+  def upcase_fields
+    self.name.upcase!
+    self.position.upcase!
+    self.area.upcase!
+    self.world.upcase!
+  end
+
+  # Verificamos que no exista una Familia con los mismos parametros
+  def family_exist
+    if(FamilyGoal.exists?(name: self.name, position: self.position,
+                            area: self.area, world: self.world))
+      errors.add(:error, 'Ya existe una Familia de Metas con eso Parametros')
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,31 @@
 class User < ApplicationRecord
+  # Antes de validar los campos convertimos los parametros a Mayusculas
+  before_validation :upcase_fields
+
+  # Posibles
+  @position = ['EJECUTIVO PERSONAS', 'AGENTE I', 'AGENTE I',
+                'AGENTE II', 'EJECUTIVO SELECT', 'GESTOR COMERCIAL SENIOR',
+                'EJECUTIVO PYME']
+
+  @area = ['ZONA SUR', 'ZONA NORTE', 'ZONA CENTRO']
+
+  @world = ['SANTANDER', 'BANEFE', 'BANCO Y FILIALES']
+
+  # Validamos los parametros
+  validates :position, inclusion: { in: @position,
+    message: "Debe seleccionar uno de los siguientes cargos #{ @position }" }
+
+  validates :area, inclusion: { in: @area,
+    message: "Debe seleccionar uno de los siguientes areas #{ @area }" }
+
+  validates :world, inclusion: { in: @world,
+    message: "Debe seleccionar uno de los siguientes mundos #{ @world }" }
+
+  def upcase_fields
+    self.name.upcase!
+    self.position.upcase!
+    self.area.upcase!
+    self.world.upcase!
+  end
+  
 end
